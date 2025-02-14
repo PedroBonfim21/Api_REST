1b9d5ea83249d7b69a0e24f9bcbc9789b6fd32b8
Pedro Bonfim <phenrique646@gmail.com>
2025-02-13 22:59:46 -0300

    Criando o CRUD de Users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,84 @@ class UserController {
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist'],
+        });
+      }
+
+      const newDataUser = await user.update(req.body);
+      return res.json(newDataUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist'],
+        });
+      }
+
+      await user.destroy();
+      return res.json({
+        msg: 'Usuario deletado da BD',
+      });
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/models/User.js
+++ src/models/User.js
@@ -9,8 +9,8 @@ export default class User extends Model {
         defaultValue: '',
         validate: {
           len: {
-            args: [4, 20],
-            msg: 'Campo nome deve ter entre 4 e 20 caracteres',
+            args: [4, 30],
+            msg: 'Campo nome deve ter entre 4 e 30 caracteres',
           },
         },
       },
@@ -44,7 +44,9 @@ export default class User extends Model {
       sequelize,
     });
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
     });
     return this;
   }
