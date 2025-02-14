76ded66f95e123b5e7a556613c0f089501d490bf
Pedro Bonfim <phenrique646@gmail.com>
2025-02-14 01:57:54 -0300

    add middleware para validar o JWT

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ['Login required'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+  const { TOKEN_SECRET } = process.env;
+  try {
+    const dados = jwt.verify(token, TOKEN_SECRET);
+    const { id, email } = dados;
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      errors: ['Token expired or invalid'],
+    });
+  }
+};
