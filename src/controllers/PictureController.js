cc89609ffcfcf621142df05e447f3099b55db9f0
Pedro Bonfim <phenrique646@gmail.com>
2025-02-18 20:02:01 -0300

    adicionando foto ao aluno

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,20 +1,28 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import Picture from '../models/Picture';
 
 const upload = multer(multerConfig).single('picture');
 
 class PictureController {
-  async create(req, res) {
-    return upload(req, res, (error) => {
+  create(req, res) {
+    return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
-      return res.json({
-        message: 'Imagem armazenada com sucesso!',
-        file: req.file.filename, // Retorna o nome do arquivo salvo
-      });
+      try {
+        const { originalname, filename } = req.file;
+        const { student_id } = req.body;
+        const picture = await Picture.create({ originalname, filename, student_id });
+
+        return res.json(picture);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno nao existe'],
+        });
+      }
     });
   }
 }
---
+++ src/models/Picture.js
@@ -0,0 +1,33 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Picture extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nao pode ser vazio',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nao pode ser vazio',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Student, { foreignKey: 'student_id' });
+  }
+}
